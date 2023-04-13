f1bfc4c632c00daaf03b9b7935c9aec86658476f
julieprokasheva <julie.prokasheva@gmail.com>
2023-04-13 11:56:16 +0300

    Add storage of available branches and areas from arxive.org.

---
+++ webscraper/website_data.py
@@ -0,0 +1,53 @@
+FIELDS = {
+    'Astrophysics':
+        {
+            'Astrophysics of Galaxies': 'GA',
+            'Cosmology and Nongalactic Astrophysics': 'CO',
+            'Earth and Planetary Astrophysics': 'EP',
+            'High Energy Astrophysical Phenomena': 'HE',
+            'Instrumentation and Methods for Astrophysics': 'IM',
+            'Solar and Stellar Astrophysics': 'SR'
+        },
+    'Condensed Matter':
+        {
+            'Disordered Systems and Neural Networks': 'dis-nn',
+            'Materials Science': 'mtrl-sci',
+            'Mesoscale and Nanoscale Physics': 'mes-hall',
+            'Other Condensed Matter':  'other',
+            'Quantum Gases': 'quant-gas',
+            'Soft Condensed Matter': 'soft',
+            'Statistical Mechanics': 'stat-mech',
+            'Strongly Correlated Electrons':  'str-el',
+            'Superconductivity': 'supr-con'
+        },
+    'Nonlinear Sciences':
+        {
+            'Adaptation and Self-Organizing Systems': 'AO',
+            'Cellular Automata and Lattice Gases': 'CG',
+            'Chaotic Dynamics': 'CD',
+            'Exactly Solvable and Integrable Systems': 'SI',
+            'Pattern Formation and Solitons': 'PS'
+        },
+    'Physics':
+        {
+            'Accelerator Physics': 'acc-ph',
+            'Applied Physics': 'app-ph',
+            'Atmospheric and Oceanic Physics': 'ao-ph',
+            'Atomic and Molecular Clusters': 'atm-clus',
+            'Atomic Physics': 'atom-ph',
+            'Biological Physics': 'bio-ph',
+            'Chemical Physics': 'chem-ph'
+        }
+}
+
+AVAILABLE_BRANCHES_PH = {
+    'Astrophysics': 'astro-ph',
+    'Condensed Matter': 'cond-mat',
+    'Quantum Physics': 'quant-ph',
+    'General Relativity and Quantum Cosmology': 'qr-qc',
+    'High Energy Physics - Experiment': 'hep-ex',
+    'Mathematical Physics': 'math-ph',
+    'Nonlinear Sciences': 'nlin',
+    'Physics': 'physics',
+    'Nuclear Theory': 'nucl-th'
+}
